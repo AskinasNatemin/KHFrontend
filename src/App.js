56e3b6cb153ba0b7bd9d725007b8c60850a45a3e
AskinasNatemin <askinas2002@gmail.com>
2025-01-23 11:04:12 +0530

    feat:new files are added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import NoPageFound from "./Components/NoPageFound";
 import Navbar from "./Components/Navbar";
+import HomePage from "./Components/HomePage";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='*' element={<NoPageFound/>}/>
-          <Route path="/" element={<Navbar/>}/>
+          <Route path="/Navbar" element={<Navbar/>}/>
+          <Route path="/" element={<HomePage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
