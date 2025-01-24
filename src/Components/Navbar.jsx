2d44414278307265b7bd0052cac85d0063a881bf
AskinasNatemin <askinas2002@gmail.com>
2025-01-24 12:45:41 +0530

    feat:new files are added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = ({handleRegistration}) => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-md NAVBAR container" style={{height:'15vh'}}>
+      <nav className="navbar navbar-expand-md NAVBAR container">
 
           <div className="ms-4 khLogo">
             <img className="navbar-brand w-100 h-100" src={logo} alt="logo" />
           </div>
 
           <button
-            className="navbar-toggler"
+            className="navbar-toggler me-2"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavAltMarkup"
@@ -26,9 +26,10 @@ const Navbar = ({handleRegistration}) => {
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse ms-3" id="navbarNavAltMarkup">
+          <div className="collapse navbar-collapse mx-3 p-1" id="navbarNavAltMarkup">
 
-            <div className="navbar-nav flex-fill  d-flex justify-content-center">
+            <div className="navbar-nav flex-fill d-flex justify-content-center">
+              
               <NavLink className="nav-link" to="/">
                 Home
               </NavLink>
@@ -39,6 +40,7 @@ const Navbar = ({handleRegistration}) => {
               <NavLink className="nav-link" to="/Contact">
                 Contact Us
               </NavLink>
+              
             </div>
            
            <div className="buttons me-4 gap-3 d-flex">
