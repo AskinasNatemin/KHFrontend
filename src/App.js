a8bf4476336734e51e2b0b4ad3e5cd4f8072f21b
Vinith70 <158442521+Vinith70@users.noreply.github.com>
2025-01-27 10:44:31 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import NoPageFound from "./Components/NoPageFound";
-import Navbar from "./Components/Navbar";
+import NoPageFound from "./components/NoPageFound";
+import Navbar from "./components/Navbar";
+import StudentLogin from "./components/Student/StudentLogin";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='*' element={<NoPageFound/>}/>
           <Route path="/" element={<Navbar/>}/>
+          <Route path="studentlogin" element={<StudentLogin/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Student/StudentLogin.jsx
+++ src/Components/Student/StudentLogin.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import "../../Styles/StudentLogin.css";
 
-const StudentLogin = () => {
+function StudentLogin() {
   return (
-    <div>StudentLogin</div>
-  )
+    <div className="student-login">
+    <div className="student-frame">
+      <form action="">
+        <h1>LOGIN</h1>
+        <div className="student-box">
+          <input type="text"  placeholder="E-Mail" />
+        </div>
+        <div className="student-box">
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="student-forgot">
+          <a href="">Forgot Password</a>
+        </div>
+        <div className="student-but">
+          <button type="submit">Login</button>
+        </div>
+        <div className="student-reg">
+          <p>
+            Don't have a account? <a href="">Register</a>
+          </p>
+        </div>
+      </form>
+    </div>
+    </div>
+  );
 }
-
-export default StudentLogin
+export default StudentLogin;
